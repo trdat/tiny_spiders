b16c2b4623ac0f9e9f0231ac37e29579ba03d601
Dat Tran <trduydat@gmail.com>
2020-08-18 23:37:17 +0700

    Update vn_covid19_1.py

--- tiny_spiders/spiders/vn_covid19_1.py
+++ tiny_spiders/spiders/vn_covid19_1.py
@@ -5,7 +5,7 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class VnCovid191Spider(scrapy.Spider):
     name = 'vn-covid19-1'
-    allowed_domains = ['ncov.moh.gov.vn']
+
     start_urls = ['https://ncov.moh.gov.vn/web/guest/dong-thoi-gian?p_p_id=101_INSTANCE_iEPhEhL1XSde&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=_118_INSTANCE_IrnCPpeHzQ4m__column-1&p_p_col_count=1&_101_INSTANCE_iEPhEhL1XSde_delta=30&_101_INSTANCE_iEPhEhL1XSde_keywords=&_101_INSTANCE_iEPhEhL1XSde_advancedSearch=false&_101_INSTANCE_iEPhEhL1XSde_andOperator=true&p_r_p_564233524_resetCur=false&_101_INSTANCE_iEPhEhL1XSde_cur=1']
 
     # rules = (
@@ -16,7 +16,7 @@ class VnCovid191Spider(scrapy.Spider):
     def parse(self, response):
         # self.log("\n----------" + str(i) + "----------\n")
         i = i + 1
-        for item in response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "portlet-content", " " ))]'):
+        for item in response.css('section.portlet'):
             yield {
                 'id': str(i),
                 'time': item.xpath('.//h3').get(),
